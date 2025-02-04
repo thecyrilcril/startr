d3f18379ffac9d71e11d345d5c1f01f033dbde6c
Cyril Fehintoluwa <cyrilcril@gmail.com>
2025-02-04 17:20:25 +0100

    chore: update unit tests

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -19,13 +19,31 @@ test('to array', function (): void {
         ]);
 });
 
-
-it('formats last name correctly when getting', function (): void {
+it('retrieves last name as a capitalized word', function (): void {
     $user = App\Models\User::factory()->create(['last_name' => 'doe'])->refresh();
     expect($user->last_name)->toBe('Doe');
 });
 
-it('formats last name correctly when setting', function (): void {
+it('it insert last name as lower case', function (): void {
     $user = App\Models\User::factory()->create(['last_name' => 'DoE'])->refresh();
     expect($user->getAttributes()['last_name'])->toBe('doe');
+});
+
+it('retrieves first name as a capitalized word', function (): void {
+    $user = App\Models\User::factory()->create(['first_name' => 'bunny'])->refresh();
+    expect($user->first_name)->toBe('Bunny');
+});
+
+it('it insert first name in lower case letters', function (): void {
+    $user = App\Models\User::factory()->create(['first_name' => 'bUnny'])->refresh();
+    expect($user->getAttributes()['first_name'])->toBe('bunny');
+});
+
+it('retrieves the full name in the right case', function (): void {
+    $user = App\Models\User::factory()->create([
+        'last_name' => 'ChRis',
+        'first_name' => 'don',
+        'other_name' => 'graham',
+    ])->refresh();
+    expect($user->full_name)->toBe('Chris Don Graham');
 });
